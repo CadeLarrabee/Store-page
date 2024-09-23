f7864252a8067d6cd136a5ae9fed70cee2062c1d
Cade Larrabee <larrabeecade@gmail.com>
2024-09-23 11:57:11 -0400

    Adding an error page & start of router

--- Shopping-Page/src/main.jsx
+++ Shopping-Page/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/HomePage.jsx";
+import ErrorPage from "./error-page";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </StrictMode>
 );
